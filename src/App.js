040590ab72d2bcb3fa7b167e0e5abcc994927331
Rishav Bharti <64918176+iamrishavb@users.noreply.github.com>
2020-12-09 15:37:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "🐶": "Dog",
+  "🐺": "Wolf",
+  "🐱": "Cat",
+  "🐵": "Monkey",
+  "🦊": "Fox",
+  "🦄": "Unicorn"
+};
+
+var animalList = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [inpt, setInpt] = useState("");
+  function changeHandler(event) {
+    var emoji = event.target.value;
+    if (emoji in emojiDictionary) {
+      setInpt(emojiDictionary[emoji]);
+    } else {
+      setInpt("We dont have this emoji");
+    }
+  }
+  function animalClickHandler(animal){
+    setInpt(emojiDictionary[animal]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Animalpedia</h1>
+      <input onChange={changeHandler}></input>
+      <div>
+        <h2>{inpt}</h2>
+      </div>
+      <h3>Animals we know</h3>
+      {animalList.map(function(animal){
+        return(
+          <span onClick={(()=> animalClickHandler(animal))}
+          key={animal} 
+          style={{padding:'1rem',fontSize:'2rem', cursor:'pointer'}}>
+          {animal}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
